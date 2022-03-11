615b1c207497e69343e43f35c75a46dc7d562396
adamsylla97 <adamsylla97@gmail.com>
2022-03-11 23:35:50 +0100

    Fix animation error in main activity

--- app/src/main/java/com/donkey/MainActivity.kt
+++ app/src/main/java/com/donkey/MainActivity.kt
@@ -3,6 +3,7 @@ package com.donkey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,6 +18,7 @@ import com.donkey.navigation.Screen
 import com.donkey.ui.Inbox.Inbox
 import com.donkey.ui.theme.DonkeyTheme
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
